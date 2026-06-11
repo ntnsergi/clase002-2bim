9ca84e617719376a6fb05689175c893efc867fcb
ntnsergi <sergi.galarzaend@gmail.com>
2026-06-11 11:50:42 -0500

    v1

---
+++ ejemplo07/academia/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from academia.models import Estudiante
+
+# Register your models here.
+
+admin.site.register(Estudiante)
---
+++ ejemplo07/academia/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    cedula = models.CharField(max_length=30, unique=True)
+    edad = models.IntegerField()
+
+# para la representacion de los objetos 
+    def __str__(self):
+        return f"{self.nombre} - {self.apellido} - {self.cedula} - {self.edad}"
